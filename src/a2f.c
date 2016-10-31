a7a9591e3b545f297027f14f2564eaebd22763e1
Gip-Gip <chmithbiz@gmail.com>
2016-10-31 19:11:33 -0400

    Fixed a2f.c

--- src/a2f.c
+++ src/a2f.c
@@ -3,11 +3,7 @@
 #define SKIP 200 /* The amount of bytes to skip in stdin */
 #define WCOR 1 /* The correction for getting an accurate minimal crestlength */
 #define THRSH 0x90 /* The threshold to which it is considered part of a crest */
-<<<<<<< HEAD
 #define RCNST 0.445 /* If a value is greater than this constant, it is rounded
-=======
-#define RCONST 0.445 /* If a value is greater than this constant, it is rounded
->>>>>>> 0acd703eb86df399d0904c145edaf457c8b58bf2
                         up */
 
 num bLen = sizeof(byte);
